ff999520c9649afbadd4e21850a028002da2fd72
MinotourDmitriy <86320101+MinotourDmitriy@users.noreply.github.com>
2024-06-26 21:38:54 +0300

    Офомление

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.all')
+
+@php
+    $title = 'Главная';
+    $description = '';
+@endphp
+
+@section('title', $title)
+@section('description', $description)
+
+@section('main')
+    <section class="section_1">
+        <div class="container">
+            <h1>!!!</h1>
+        </div>
+    </section>
+@endsection
