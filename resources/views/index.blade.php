720c716ae495a93d54413df4d9a0e3494073a849
MinotourDmitriy <86320101+MinotourDmitriy@users.noreply.github.com>
2024-07-01 13:16:39 +0300

    Сверстан блок футера

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -420,4 +420,36 @@
             </div>
         </div>
     </section>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__wrapper">
+                <a href="#"><img class="footer__logo" src="{{ asset('/img/icons/logo-icon.svg') }}"
+                        alt="Логотип Профмедика"></a>
+                <nav>
+                    <ul class="footer__menu menu">
+                        <li class="menu__items"><a class="item" href="#">Оборудование в аренду</a></li>
+                        <li class="menu__items"><a class="item" href="#">Купить оборудование</a>
+                        </li>
+                        <li class="menu__items"><a class="item" href="#">Контакты</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul class="footer__menu menu">
+                        <li class="menu__items"><a class="item" href="#">Политика в области обаботки персональных
+                                данных</a></li>
+                        <li class="menu__items"><a class="item" href="#">Согласие на обработку
+                                персональных данных</a>
+                        </li>
+                    </ul>
+                </nav>
+                <a href="tel:+79090000000" class="menu__call">+7 909 000 00 00</a>
+
+                <button class="menu__mobile-button">
+                    <img src="{{ asset('/img/icons/menu-icon.svg') }}" alt="Мобильное меню">
+                </button>
+            </div>
+        </div>
+    </footer>
 @endsection
