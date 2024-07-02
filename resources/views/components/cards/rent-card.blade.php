505c27e4de02ca71ef9530f457d5be5d48236c0e
asmi046 <asmi046@gmail.com>
2024-07-02 23:33:51 +0300

    Карточка аренды

---
+++ resources/views/components/cards/rent-card.blade.php
@@ -0,0 +1,26 @@
+<div class="card__item">
+    <div class="card__item-img">
+        <img src="{{ asset('/img/card-items/hodynki_na_kolesax.png') }}"
+            alt="Фото оборудования ходунков на колёсах производства R Wheel">
+    </div>
+    <div class="card__item-info">
+        <div class="card__item_equipment">
+            Ходунки на колесах R Wheel
+        </div>
+        <div class="card__item__rent">
+            <div class="item__rent">
+                <div class="item__rent_time">Сутки:</div>
+                <div class="item__rent_price">300 руб</div>
+            </div>
+            <div class="item__rent">
+                <div class="item__rent_time">Неделя:</div>
+                <div class="item__rent_price">1000 руб</div>
+            </div>
+            <div class="item__rent">
+                <div class="item__rent_time">Месяц:</div>
+                <div class="item__rent_price">7000 руб</div>
+            </div>
+        </div>
+        <button class="button">Взять в аренду</button>
+    </div>
+</div>
