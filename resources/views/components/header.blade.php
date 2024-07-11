6e92bec049e894c39477380a29626578ee5e5a99
asmi046 <asmi046@gmail.com>
2024-07-11 21:33:45 +0300

    +1

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
 <header class="header">
     <div class="container container__header">
         <div class="header__wrapper">
-            <a href="#"><img class="header__logo logo" src="{{ asset('/img/icons/logo-icon.svg') }}"
-                    alt="Логотип Профмедика"></a>
+            <a href="#"><img class="header__logo logo" src="{{ asset('/img/icons/logo-icon.svg') }}" alt="Логотип Профмедика"></a>
+
             <nav class="header__navigation navigation">
                 <x-menu-navigation></x-menu-navigation>
             </nav>
