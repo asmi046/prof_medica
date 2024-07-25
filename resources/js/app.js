b7ca9220a2f79434af0da8e3f4756098700e0b4e
MinotourDmitriy <Kkunkka1@mail.ru>
2024-07-25 21:46:49 +0300

    Меню шапки на одном уровне, картинка главная, скролл-бордер,преимущества,центр подвала

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+const headerBorder = document.querySelector('.container__header')
+window.addEventListener('scroll', function(e){
+    const scrollInfo = document.documentElement.scrollTop;
+    if(scrollInfo>10){
+        headerBorder.style.borderBottom = '2px solid black';
+    }  else {
+        headerBorder.style.borderBottom = '';
+    }
+});
