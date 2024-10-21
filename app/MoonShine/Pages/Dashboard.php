91995111c724d38197e62dfd2de87332062ffe6f
asmi046 <asmi046@gmail.com>
2024-10-21 18:25:24 +0300

    Дашборд

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages;
 
+use App\Models\Product;
+use App\Models\Category;
 use MoonShine\Pages\Page;
+use App\Models\ProductRental;
+use MoonShine\Decorations\Grid;
+use MoonShine\Metrics\ValueMetric;
 use MoonShine\Components\MoonShineComponent;
 
 class Dashboard extends Page
@@ -21,7 +26,7 @@ class Dashboard extends Page
 
     public function title(): string
     {
-        return $this->title ?: 'Dashboard';
+        return $this->title ?: 'Сайт Профмедика';
     }
 
     /**
@@ -29,6 +34,20 @@ class Dashboard extends Page
      */
     public function components(): array
 	{
-		return [];
+		return [
+            Grid::make([
+
+                ValueMetric::make("Товары в аренду")
+                    ->value(Product::all()->count())
+                    ->columnSpan(4),
+                ValueMetric::make("Товары на продажу")
+                    ->value(ProductRental::all()->count())
+                    ->columnSpan(4),
+                ValueMetric::make("Категорий")
+                    ->value(Category::all()->count())
+                    ->columnSpan(4),
+
+            ]),
+        ];
 	}
 }
